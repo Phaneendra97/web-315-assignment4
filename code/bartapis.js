e46e4cd9fd1c3af2c25c21acb0211a79db674cf9
phaneendra97 <phaneendra1997@gmail.com>
2023-03-05 11:17:18 -0800

    temp

--- code/bartapis.js
+++ code/bartapis.js
@@ -1,4 +1,5 @@
-const axios = require("axios");
+const https = require("https");
+const xml2js = require("xml2js");
 
 module.exports = function (app) {
   const bodyParser = require("body-parser");
@@ -15,76 +16,190 @@ module.exports = function (app) {
     next();
   });
 
-  app.get("/stations", (req, apiResponse) => {
-    axios
-      .get(
-        "https://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V&json=y"
-      )
+  // app.get("/stations", (req, apiResponse) => {
+  //   axios
+  //     .get(
+  //       "https://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V&json=y"
+  //     )
 
-      // Show response data
-      .then((res) => {
-        apiResponse.send({
-          data: res.data.root.stations,
+  //     // Show response data
+  //     .then((res) => {
+  //       const buffer = Buffer.from(res.data);
+  //       let jsonData = xmlParser.toJson(buffer);
+  //       console.log(jsonData);
+  //       apiResponse.send({
+  //         data: res.data.root.stations,
+  //       });
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //       apiResponse.status(200);
+  //       apiResponse.send({
+  //         error: JSON.stringify(err),
+  //       });
+  //     });
+  // });
+
+  // app.get("/trips/source/:srcaddr/dest/:destaddr", function (req, apiResponse) {
+  //   var source = req.params["srcaddr"];
+  //   var destination = req.params["destaddr"];
+  //   console.log(source);
+  //   console.log(destination);
+  //   const url =
+  //     "https://api.bart.gov/api/sched.aspx?cmd=depart&orig=" +
+  //     source +
+  //     "&dest=" +
+  //     destination +
+  //     "&date=now&key=MW9S-E7SL-26DU-VV8V&b=2&a=2&l=1&json=y";
+  //   console.log(url);
+  //   axios
+  //     .get(url)
+
+  //     // Show response data
+  //     .then((res) => {
+  //       console.log(res.data);
+  //       apiResponse.send({
+  //         trip: res.data.root,
+  //       });
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //       apiResponse.status(200);
+  //       apiResponse.send({
+  //         error: JSON.stringify(err),
+  //       });
+  //     });
+  // });
+
+  // app.get("/station/source/:source", function (req, apiResponse) {
+  //   var source = req.params["source"];
+  //   axios
+  //     .get(
+  //       "https://api.bart.gov/api/stn.aspx?cmd=stninfo&orig=" +
+  //         source +
+  //         "&key=MW9S-E7SL-26DU-VV8V&json=y"
+  //     )
+
+  //     // Show response data
+  //     .then((res) => {
+  //       apiResponse.send({
+  //         station: res.data.root.stations.station,
+  //       });
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //       apiResponse.status(200);
+  //       apiResponse.send({
+  //         error: JSON.stringify(err),
+  //       });
+  //     });
+  // });
+
+  app.get("/stationsTest", (req, apiResponse) => {
+    const url =
+      "https://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V";
+
+    https
+      .get(url, (res) => {
+        let data = "";
+
+        res.on("data", (chunk) => {
+          data += chunk;
+        });
+
+        res.on("end", () => {
+         
+          // parse response and add child elements
+          xml2js.parseString(data, (err, result) => {
+            if (err) {
+              console.error(err);
+            } else {
+              // Convert the JavaScript object to JSON
+              apiResponse.status(200);
+              apiResponse.send({
+                data: result,
+              });
+            }
+          });
         });
       })
-      .catch((err) => {
-        console.log(err);
-        apiResponse.status(200);
-        apiResponse.send({
-          error: JSON.stringify(err),
-        });
+      .on("error", (err) => {
+        console.error(err);
       });
   });
 
-  app.get("/trips/source/:srcaddr/dest/:destaddr", function (req, apiResponse) {
+  app.get("/stationTest/source/:source", function (req, apiResponse) {
+    var source = req.params["source"];
+    const url =
+      "https://api.bart.gov/api/stn.aspx?cmd=stninfo&orig=" +
+      source +
+      "&key=MW9S-E7SL-26DU-VV8V";
+      https
+      .get(url, (res) => {
+        let data = "";
+
+        res.on("data", (chunk) => {
+          data += chunk;
+        });
+
+        res.on("end", () => {
+          // create XML root element
+
+          // parse response and add child elements
+          xml2js.parseString(data, (err, result) => {
+            if (err) {
+              console.error(err);
+            } else {
+              // Convert the JavaScript object to JSON
+              apiResponse.status(200);
+              apiResponse.send({
+                station: result.root.stations[0].station,
+              });
+            }
+          });
+        });
+      })
+      .on("error", (err) => {
+        console.error(err);
+      });
+  });
+
+  app.get("/tripsTest/source/:srcaddr/dest/:destaddr", function (req, apiResponse) {
     var source = req.params["srcaddr"];
     var destination = req.params["destaddr"];
-    console.log(source);
-    console.log(destination);
-    const url = "https://api.bart.gov/api/sched.aspx?cmd=depart&orig=" +source+"&dest="+destination+"&date=now&key=MW9S-E7SL-26DU-VV8V&b=2&a=2&l=1&json=y"
-    console.log(url);
-    axios
-      .get(
-        url
-      )
+    const url =
+    "https://api.bart.gov/api/sched.aspx?cmd=depart&orig=" +
+    source +
+    "&dest=" +
+    destination +
+    "&date=now&key=MW9S-E7SL-26DU-VV8V&b=2&a=2&l=1";
+      https
+      .get(url, (res) => {
+        let data = "";
 
-      // Show response data
-      .then((res) => {
-        console.log(res.data);
-        apiResponse.send({
-          trip: res.data.root,
+        res.on("data", (chunk) => {
+          data += chunk;
+        });
+
+        res.on("end", () => {
+          // create XML root element
+
+          // parse response and add child elements
+          xml2js.parseString(data, (err, result) => {
+            if (err) {
+              console.error(err);
+            } else {
+              // Convert the JavaScript object to JSON
+              apiResponse.status(200);
+              apiResponse.send({
+                trip: result.root,
+              });
+            }
+          });
         });
       })
-      .catch((err) => {
-        console.log(err);
-        apiResponse.status(200);
-        apiResponse.send({
-          error: JSON.stringify(err),
-        });
-      });
-  });
-
-  app.get("/station/source/:source", function (req, apiResponse) {
-    var source = req.params["source"];
-    axios
-      .get(
-        "https://api.bart.gov/api/stn.aspx?cmd=stninfo&orig=" +
-          source +
-          "&key=MW9S-E7SL-26DU-VV8V&json=y"
-      )
-
-      // Show response data
-      .then((res) => {
-        apiResponse.send({
-          station: res.data.root.stations.station,
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-        apiResponse.status(200);
-        apiResponse.send({
-          error: JSON.stringify(err),
-        });
+      .on("error", (err) => {
+        console.error(err);
       });
   });
 };
--- code/server.js
+++ code/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 5000;
 require("./bartapis")(app);
+// require("./api")(app);
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
